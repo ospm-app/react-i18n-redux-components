d4dad8cd01c07cd0a28f11aff394af7d46d8a473
Alexey Lyakhov <justfly1984@gmail.com>
2025-05-08 13:47:56 +0700

    cleanup

--- example/app/state/reducers/forms.ts
+++ example/app/state/reducers/forms.ts
@@ -3,80 +3,6 @@ import { createAction, createReducer } from '@reduxjs/toolkit';
 import { mergeDeep } from 'utils/object.ts';
 
 import type {
-  Flags,
-  UserResponse,
-  CreateOrganizationResponse
-} from 'app/valibot.ts';
-
-import {
-  resetContact,
-  type ContactForm,
-  fetchSuccessContact,
-  initialFieldsContact,
-  type ContactFieldNames
-} from 'state/reducers/forms/contact.ts';
-import {
-  resetForgot,
-  type ForgotForm,
-  fetchSuccessForgot,
-  initialFieldsForgot,
-  type ForgotFieldNames
-} from 'state/reducers/forms/forgot.ts';
-import {
-  resetLogIn,
-  type LogInForm,
-  fetchSuccessLogIn,
-  initialFieldsLogIn,
-  type LogInFieldNames
-} from 'state/reducers/forms/log-in.ts';
-import {
-  resetProfilePassword,
-  type ProfilePasswordForm,
-  fetchSuccessProfilePassword,
-  initialFieldsProfilePassword,
-  type ProfilePasswordFieldNames
-} from 'state/reducers/forms/profile-password.ts';
-import {
-  logInProfilePersonal,
-  resetProfilePersonal,
-  type ProfilePersonalForm,
-  fetchSuccessProfilePersonal,
-  initialFieldsProfilePersonal,
-  profilePersonalLastNameUpdate,
-  type ProfilePersonalFieldNames,
-  profilePersonalFirstNameUpdate,
-  type ProfilePersonalUpdateLastNamePayload,
-  type ProfilePersonalUpdateFirstNamePayload
-} from 'state/reducers/forms/profile-personal.ts';
-import {
-  resetProfileDetails,
-  type ProfileDetailsForm,
-  fetchSuccessProfileDetails,
-  initialFieldsProfileDetails,
-  type ProfileDetailsFieldNames
-} from 'state/reducers/forms/profile-details.ts';
-import {
-  resetReset,
-  type ResetForm,
-  fetchSuccessReset,
-  initialFieldsReset,
-  type ResetFieldNames
-} from 'state/reducers/forms/reset.ts';
-import {
-  resetSignUpWithGithubSuccess,
-  type SignUpWithGithubSuccessForm,
-  fetchSuccessSignUpWithGithubSuccess,
-  initialFieldsSignUpWithGithubSuccess,
-  type SignUpWithGithubSuccessFieldNames
-} from 'state/reducers/forms/sign-up-with-github-success.ts';
-import {
-  resetSignUp,
-  type SignUpForm,
-  fetchSuccessSignUp,
-  initialFieldsSignUp,
-  type SignUpFieldNames
-} from 'state/reducers/forms/sign-up.ts';
-import type {
   SetInPayload,
   ValidatePayload,
   ChangeInputPayload,
@@ -115,54 +41,6 @@ import {
   initialFieldsUiKit,
   type UiKitFieldNames
 } from 'state/reducers/forms/ui-kit.ts';
-import {
-  resetProfileCountry,
-  type ProfileCountryForm,
-  fetchSuccessProfileCountry,
-  initialFieldsProfileCountry,
-  profileCountryCountryUpdate,
-  type ProfileCountryFieldNames,
-  type ProfileCountryUpdateCountryPayload
-} from 'state/reducers/forms/profile-country.ts';
-import {
-  resetProfilePhone,
-  type ProfilePhoneForm,
-  profilePhonePhoneUpdate,
-  fetchSuccessProfilePhone,
-  initialFieldsProfilePhone,
-  type ProfilePhoneFieldNames,
-  type ProfilePhoneUpdatePhonePayload
-} from 'state/reducers/forms/profile-phone.ts';
-import {
-  resetProfileUsername,
-  type ProfileUsernameForm,
-  fetchSuccessProfileUsername,
-  initialFieldsProfileUsername,
-  profileUsernameUsernameUpdate,
-  type ProfileUsernameFieldNames,
-  type ProfileUsernameUpdateUsernamePayload
-} from 'state/reducers/forms/profile-username.ts';
-import {
-  resetCreateOrganization,
-  type CreateOrganizationForm,
-  fetchSuccessCreateOrganization,
-  initialFieldsCreateOrganization,
-  type CreateOrganizationFieldNames
-} from 'state/reducers/forms/create-organization.ts';
-import {
-  resetDashboardUsername,
-  type DashboardUsernameForm,
-  fetchSuccessDashboardUsername,
-  initialFieldsDashboardUsername,
-  type DashboardUsernameFieldNames
-} from 'state/reducers/forms/dashboard-username.ts';
-import {
-  resetInviteOrganizationMember,
-  type InviteOrganizationMemberForm,
-  fetchSuccessInviteOrganizationMember,
-  initialFieldsInviteOrganizationMember,
-  type InviteOrganizationMemberFieldNames
-} from 'state/reducers/forms/invite-organization-member.ts';
 
 function getFormField<F extends FormFields>(
   form: F,
@@ -184,50 +62,9 @@ function getFormField<F extends FormFields>(
 
 export type FieldNames =
   | UiKitFieldNames
-  | ContactFieldNames
-  | LogInFieldNames
-  | SignUpFieldNames
-  | SignUpWithGithubSuccessFieldNames
-  | ForgotFieldNames
-  | ResetFieldNames
-  | ContactFieldNames
-  | ProfilePasswordFieldNames
-  | ProfilePersonalFieldNames
-  | ProfileDetailsFieldNames
-  | ProfileUsernameFieldNames
-  | ProfilePhoneFieldNames
-  | ProfileCountryFieldNames
-  | CreateOrganizationFieldNames
-  | DashboardUsernameFieldNames
-  | InviteOrganizationMemberFieldNames
 
 type FormFields =
   | UiKitForm
-  | ContactForm
-  | LogInForm
-  | SignUpForm
-  | SignUpWithGithubSuccessForm
-  | ForgotForm
-  | ResetForm
-  | ContactForm
-  | ProfilePasswordForm
-  | ProfilePersonalForm
-  | ProfileDetailsForm
-  | ProfileUsernameForm
-  | ProfilePhoneForm
-  | ProfileCountryForm
-  | CreateOrganizationForm
-  | DashboardUsernameForm
-  | InviteOrganizationMemberForm
-
-export const logIn = createAction<
-  Omit<UserResponse, 'success' | 'visitorId' | 'interactionId'>,
-  'logIn'
->('logIn');
-type LogInAction = ReturnType<typeof logIn>
-
-export const logOut = createAction<undefined, 'logOut'>('logOut');
-type LogOutAction = ReturnType<typeof logOut>
 
 export const fetchRequest = createAction<FetchRequestPayload, 'fetchRequest'>(
   'fetchRequest'
@@ -249,71 +86,6 @@ export const fetchComplete = createAction<
   'fetchComplete'
 >('fetchComplete');
 type FetchCompleteAction = ReturnType<typeof fetchComplete>
-
-// Profile
-
-export type ProfileValuesPayload = {
-  firstName: string
-  lastName: string
-  username: string
-  email: string
-  enable2fa: boolean
-  isVerified: boolean
-  country: Flags | ''
-  phone: string
-  githubAccountSync: boolean
-  accountLastUsed?: string | undefined
-  createdAt: string
-  updatedAt: string
-}
-
-export const updateProfileValues = createAction<
-  ProfileValuesPayload,
-  'updateProfileValues'
->('updateProfileValues');
-type UpdateProfileValuesAction = ReturnType<typeof updateProfileValues>
-
-export const profilePersonalUpdateFirstName = createAction<
-  ProfilePersonalUpdateFirstNamePayload,
-  'profilePersonalUpdateFirstName'
->('profilePersonalUpdateFirstName');
-type ProfilePersonalUpdateFirstNameAction = ReturnType<
-  typeof profilePersonalUpdateFirstName
->
-
-export const profilePersonalUpdateLastName = createAction<
-  ProfilePersonalUpdateLastNamePayload,
-  'profilePersonalUpdateLastName'
->('profilePersonalUpdateLastName');
-
-type ProfilePersonalUpdateLastNameAction = ReturnType<
-  typeof profilePersonalUpdateLastName
->
-
-export const profileUsernameUpdateUsername = createAction<
-  ProfileUsernameUpdateUsernamePayload,
-  'profileUsernameUpdateUsername'
->('profileUsernameUpdateUsername');
-
-type ProfileUsernameUpdateUsernameAction = ReturnType<
-  typeof profileUsernameUpdateUsername
->
-
-export const profilePhoneUpdatePhone = createAction<
-  ProfilePhoneUpdatePhonePayload,
-  'profilePhoneUpdatePhone'
->('profilePhoneUpdatePhone');
-
-type ProfilePhoneUpdatePhoneAction = ReturnType<typeof profilePhoneUpdatePhone>
-
-export const profileCountryUpdateCountry = createAction<
-  ProfileCountryUpdateCountryPayload,
-  'profileCountryUpdateCountry'
->('profileCountryUpdateCountry');
-
-type ProfileCountryUpdateCountryAction = ReturnType<
-  typeof profileCountryUpdateCountry
->
 
 // Library
 
@@ -543,30 +315,8 @@ type ValidateRangePickerTimepickerInputEndAction = ReturnType<
   typeof validateRangePickerTimepickerEndInput
 >
 
-// Organization
-
-export const createOrganization = createAction<
-  Omit<CreateOrganizationResponse, 'success' | 'visitorId' | 'interactionId'>,
-  'createOrganization'
->('createOrganization');
-
 const initialState = {
   uiKit: initialFieldsUiKit,
-  forgot: initialFieldsForgot,
-  signUp: initialFieldsSignUp,
-  logIn: initialFieldsLogIn,
-  reset: initialFieldsReset,
-  contact: initialFieldsContact,
-  profileDetails: initialFieldsProfileDetails,
-  profilePassword: initialFieldsProfilePassword,
-  profilePersonal: initialFieldsProfilePersonal,
-  profileUsername: initialFieldsProfileUsername,
-  profilePhone: initialFieldsProfilePhone,
-  profileCountry: initialFieldsProfileCountry,
-  signUpWithGithubSuccess: initialFieldsSignUpWithGithubSuccess,
-  createOrganization: initialFieldsCreateOrganization,
-  dashboardUsername: initialFieldsDashboardUsername,
-  inviteOrganizationMember: initialFieldsInviteOrganizationMember
 } as const;
 
 export type FormsState = typeof initialState
@@ -586,29 +336,6 @@ export const reducer = createReducer(initialState, (builder) => {
         success,
         intlSuccess
       });
-    })
-    .addCase(logIn, (draft, action: LogInAction): void => {
-      logInProfilePersonal(draft.profilePersonal, action.payload);
-
-      resetLogIn(draft.logIn);
-    })
-
-    .addCase(logOut, (draft, _action: LogOutAction): void => {
-      resetLogIn(draft.logIn);
-      resetReset(draft.reset);
-      resetForgot(draft.forgot);
-      resetSignUp(draft.signUp);
-      resetContact(draft.contact);
-      resetProfilePhone(draft.profilePhone);
-      resetProfileDetails(draft.profileDetails);
-      resetProfileCountry(draft.profileCountry);
-      resetProfilePassword(draft.profilePassword);
-      resetProfilePersonal(draft.profilePersonal);
-      resetProfileUsername(draft.profileUsername);
-      resetDashboardUsername(draft.dashboardUsername);
-      resetCreateOrganization(draft.createOrganization);
-      resetSignUpWithGithubSuccess(draft.signUpWithGithubSuccess);
-      resetInviteOrganizationMember(draft.inviteOrganizationMember);
     })
     .addCase(
       changeInput,
@@ -1003,158 +730,8 @@ export const reducer = createReducer(initialState, (builder) => {
 
       defaultFetchRequest(reducer, action.payload);
     })
-    .addCase(fetchSuccess, (draft, action: FetchSuccessAction): void => {
+    .addCase(fetchSuccess, (_draft, action: FetchSuccessAction): void => {
       switch (action.payload.path[0]) {
-        case 'logIn': {
-          fetchSuccessLogIn(draft.logIn, {
-            path: ['logIn'],
-            success: action.payload.success,
-            intlSuccess: action.payload.intlSuccess
-          });
-
-          break;
-        }
-
-        case 'signUp': {
-          fetchSuccessSignUp(draft.signUp, {
-            path: ['signUp'],
-            success: action.payload.success,
-            intlSuccess: action.payload.intlSuccess
-          });
-
-          break;
-        }
-
-        case 'signUpWithGithubSuccess': {
-          fetchSuccessSignUpWithGithubSuccess(draft.signUpWithGithubSuccess, {
-            path: ['signUpWithGithubSuccess'],
-            success: action.payload.success,
-            intlSuccess: action.payload.intlSuccess
-          });
-
-          break;
-        }
-
-        case 'reset': {
-          fetchSuccessReset(draft.reset, {
-            path: ['reset'],
-            success: action.payload.success,
-            intlSuccess: action.payload.intlSuccess
-          });
-
-          break;
-        }
-
-        case 'forgot': {
-          fetchSuccessForgot(draft.forgot, {
-            path: ['forgot'],
-            success: action.payload.success,
-            intlSuccess: action.payload.intlSuccess
-          });
-
-          break;
-        }
-
-        case 'contact': {
-          fetchSuccessContact(draft.contact, {
-            path: ['contact'],
-            success: action.payload.success,
-            intlSuccess: action.payload.intlSuccess
-          });
-
-          break;
-        }
-
-        case 'profilePassword': {
-          fetchSuccessProfilePassword(draft.profilePassword, {
-            path: ['profilePassword'],
-            success: action.payload.success,
-            intlSuccess: action.payload.intlSuccess
-          });
-
-          break;
-        }
-
-        case 'profilePersonal': {
-          fetchSuccessProfilePersonal(draft.profilePersonal, {
-            path: ['profilePersonal'],
-            success: action.payload.success,
-            intlSuccess: action.payload.intlSuccess
-          });
-
-          break;
-        }
-
-        case 'profileDetails': {
-          fetchSuccessProfileDetails(draft.profileDetails, {
-            path: ['profileDetails'],
-            success: action.payload.success,
-            intlSuccess: action.payload.intlSuccess
-          });
-
-          break;
-        }
-
-        case 'profileUsername': {
-          fetchSuccessProfileUsername(draft.profileUsername, {
-            path: ['profileUsername'],
-            success: action.payload.success,
-            intlSuccess: action.payload.intlSuccess
-          });
-
-          break;
-        }
-
-        case 'profilePhone': {
-          fetchSuccessProfilePhone(draft.profilePhone, {
-            path: ['profilePhone'],
-            success: action.payload.success,
-            intlSuccess: action.payload.intlSuccess
-          });
-
-          break;
-        }
-
-        case 'profileCountry': {
-          fetchSuccessProfileCountry(draft.profileCountry, {
-            path: ['profileCountry'],
-            success: action.payload.success,
-            intlSuccess: action.payload.intlSuccess
-          });
-
-          break;
-        }
-
-        case 'createOrganization': {
-          fetchSuccessCreateOrganization(draft.createOrganization, {
-            path: ['createOrganization'],
-            success: action.payload.success,
-            intlSuccess: action.payload.intlSuccess
-          });
-
-          break;
-        }
-
-        case 'dashboardUsername': {
-          fetchSuccessDashboardUsername(draft.dashboardUsername, {
-            path: ['dashboardUsername'],
-            success: action.payload.success,
-            intlSuccess: action.payload.intlSuccess
-          });
-
-          break;
-        }
-
-        case 'inviteOrganizationMember': {
-          fetchSuccessInviteOrganizationMember(draft.inviteOrganizationMember, {
-            path: ['inviteOrganizationMember'],
-            success: action.payload.success,
-            intlSuccess: action.payload.intlSuccess
-          });
-
-          break;
-        }
-
         default: {
           break;
         }
@@ -1175,41 +752,6 @@ export const reducer = createReducer(initialState, (builder) => {
         isFetching: false
       });
     })
-
-    .addCase(
-      profilePersonalUpdateFirstName,
-      (draft, action: ProfilePersonalUpdateFirstNameAction): void => {
-        profilePersonalFirstNameUpdate(draft.profilePersonal, action.payload);
-      }
-    )
-
-    .addCase(
-      profilePersonalUpdateLastName,
-      (draft, action: ProfilePersonalUpdateLastNameAction): void => {
-        profilePersonalLastNameUpdate(draft.profilePersonal, action.payload);
-      }
-    )
-
-    .addCase(
-      profileUsernameUpdateUsername,
-      (draft, action: ProfileUsernameUpdateUsernameAction): void => {
-        profileUsernameUsernameUpdate(draft.profileUsername, action.payload);
-      }
-    )
-
-    .addCase(
-      profilePhoneUpdatePhone,
-      (draft, action: ProfilePhoneUpdatePhoneAction): void => {
-        profilePhonePhoneUpdate(draft.profilePhone, action.payload);
-      }
-    )
-
-    .addCase(
-      profileCountryUpdateCountry,
-      (draft, action: ProfileCountryUpdateCountryAction): void => {
-        profileCountryCountryUpdate(draft.profileCountry, action.payload);
-      }
-    )
 
     .addCase(changeCheckbox, (draft, action: ChangeCheckboxAction): void => {
       const { checked, valid, invalid } = action.payload;
@@ -1410,34 +952,6 @@ export const reducer = createReducer(initialState, (builder) => {
         }
       }
     )
-
-    .addCase(
-      updateProfileValues,
-      (draft, action: UpdateProfileValuesAction): void => {
-        draft.profilePersonal.email.value = action.payload.email;
-        draft.profilePersonal.firstName.value = action.payload.firstName;
-        draft.profilePersonal.lastName.value = action.payload.lastName;
-
-        draft.profileUsername.username.value = action.payload.username;
-
-        draft.profileCountry.country.value = action.payload.country;
-
-        draft.profilePhone.phone.value = action.payload.phone;
-
-        draft.profileDetails.enable2fa.checked = action.payload.enable2fa;
-        draft.profileDetails.isVerified.checked = action.payload.isVerified;
-        draft.profileDetails.githubAccountSync.checked =
-          action.payload.githubAccountSync;
-        draft.profileDetails.accountLastUsed.value =
-          action.payload.accountLastUsed ?? '';
-        draft.profileDetails.createdAt.value = action.payload.createdAt;
-        draft.profileDetails.updatedAt.value = action.payload.updatedAt;
-      }
-    )
-
-    .addCase(createOrganization, (draft): void => {
-      resetCreateOrganization(draft.createOrganization);
-    });
 });
 
 function defaultFetchRequest(
